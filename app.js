9d15e723eaac3354f92326f147e929d9c561611e
parsachow <parsakishwa06@gmail.com>
2023-06-23 19:07:02 -0400

    b solved

---
+++ app.js
@@ -0,0 +1,18 @@
+// Create a variable called firstVariable.
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number.
+// Store the value of firstVariable in a new variable called secondVariable
+// Change the value of secondVariable to any string.
+// What is the value of firstVariable?
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+
+let firstVariable; 
+firstVariable = "Hello World";
+firstVariable = 342;
+let secondVariable = firstVariable;
+secondVariable = "Change to any string";
+console.log(firstVariable);
+let yourName = "Parsa Chowdhury";
+console.log(`Hello, my name is ${yourName}`);
+
+
